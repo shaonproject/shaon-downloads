de345ced882b0e2551d7537e3b1235446671d402
shaonproject <153928138+shaonproject@users.noreply.github.com>
2024-08-24 15:36:23 +0600

    Update main.js

--- routes/main.js
+++ routes/main.js
@@ -4,6 +4,6 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', (req, res) => {
-    res.redirect('https://github.com/shaon/shaon-downloads#readme')
+    res.redirect('https://github.com/shaonproject/shaon-downloads#readme')
 })
 module.exports = router
